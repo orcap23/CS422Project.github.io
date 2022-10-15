e71ee87b996dd67215bb5fb4b85d0229eece821e
Madeline Porcaro <meporca@Madelines-MacBook-Pro.local>
2022-10-15 14:00:08 -0700

    fixed socket import

--- tsfc/forecastingapp/forms.py
+++ tsfc/forecastingapp/forms.py
@@ -1,4 +1,3 @@
 from django import forms
-from socket import fromshare
 
 #class UploadFileForm(forms.Form):
--- tsfc/forecastingapp/views.py
+++ tsfc/forecastingapp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.db import transaction
-from .forms import UploadFileForm
 
 def say_sth(request):
     return render(request, 'index.html', {'name': 'Team 2!!!'})
