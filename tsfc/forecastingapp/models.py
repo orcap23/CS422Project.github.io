bf9e1a749e16ea9b6c0e31d33ad2bd2a246a24a5
Luying Cai <luyingcai@Luyings-MacBook-Pro.local>
2022-10-15 19:08:26 -0700

    model changed

--- tsfc/forecastingapp/models.py
+++ tsfc/forecastingapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-#A Django model is a table in your database.
+#https://docs.djangoproject.com/zh-hans/4.1/topics/db/models/
+#https://stackoverflow.com/questions/17821400/regex-match-for-domain-name-in-django-model
+
+class Tsmetadata(models.Model):
+    tsname = models.CharField(max_length=255, primary_key=True)
+    description = models.TextField()
+    domain = models.CharField(max_length=255)
+    units = models.IntegerField()
+    keywords = models.CharField(max_length=255)
+    scalar = models.IntegerField()
+    Length = models.IntegerField()
+    SamplingPeriod = models.DecimalField(max_digits=6, decimal_places=2)
+
+class Tsdata(models.Model):
+    date = models.DateField(null=True)
+    time = models.IntegerField()
+    magnitude = models.DecimalField(max_digits=6, decimal_places=2)
+    tstime = models.OneToOneField(Tsmetadata, on_delete=models.CASCADE)
+
+
+
+
+
+
+
+
+
+
+    
--- tsfc/forecastingapp/views.py
+++ tsfc/forecastingapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+<<<<<<< HEAD
 from django.db import transaction
+=======
+
+>>>>>>> 005d403 (model changed)
 
 def say_sth(request):
     return render(request, 'index.html', {'name': 'Team 2!!!'})
