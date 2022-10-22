13acb4a83391a675861a091940c7a199f1bab5c3
Savina Tsai <52936556+cailucky777@users.noreply.github.com>
2022-10-22 11:40:13 -0700

    Add files via upload
    upload part most finished

---
+++ CS422Project.github.io/tsfc/forecastingapp/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+import os
+import uuid
+
+def user_directory_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = '{}.{}'.format(uuid.uuid4().hex[:10], ext)
+    return os.path.join("files", filename)
+
+class File(models.Model):
+    file = models.FileField(upload_to=user_directory_path, null=True)
+    upload_method = models.CharField(max_length=20, verbose_name="Upload Method")
+
+
+
+
+
+
+
+
+
+
+    
